d1436ef298d0b35121bca4dc83bb4a2019653885
PREM KUMAR R <pkrprem2005@gmail.com>
2025-03-15 23:14:13 +0530

    in the process of solving pyimage error

--- app_lite.py
+++ app_lite.py
@@ -233,12 +233,29 @@ def setup_ui():
     ctk.CTkButton(sign_in_frame,
                    text="Instagram", command= open_insta).grid(row=2, column=1, pady=5, padx=5)
 
+def get_back_to_light_mode():
+    if ctk.get_appearance_mode() == "Dark":
+        switch_event()
+    else :
+        None
+
+def fade_out_close(alpha=1.0):
+    if alpha > 0:
+        root.attributes("-alpha", alpha)
+        root.after(50, fade_out_close, alpha - 0.05)
+    else:
+        get_back_to_light_mode()
+        for task in root.tk.call("after", "info"):
+            root.after_cancel(task)
+        root.destroy()
+
 if __name__ == "__main__" :
     root = ctk.CTk()
     ctk.set_appearance_mode("light")
     ctk.set_default_color_theme("dark-blue")
     root.title("KairosPredict/login")
-    root.geometry("345x580")   
+    root.geometry("345x580")
+    root.protocol("WM_DELETE_WINDOW", lambda: fade_out_close())   
     root.iconbitmap(r"static\images\icon.ico")
     setup_ui()
     current_user = get_user()
